dad554b3857b6f4d671bf2290ba7694cc068f3e9
Pooja Mandal <mandalpooja1238@gmail.com>
2026-07-09 01:58:12 +0530

    Completed Exercise 7 - Financial Forecasting

---
+++ Data Structures and Algorithms/Exercise 7 - Financial Forecasting/FinancialForecasting/ForecastCalculator.cs
@@ -0,0 +1,26 @@
+﻿namespace FinancialForecasting
+{
+    // This class contains the method for calculating future value
+    internal class ForecastCalculator
+    {
+        // Method to calculate future value using recursion
+        public double CalculateFutureValue(
+            double currentValue,
+            double growthRate,
+            int years)
+        {
+            // Base case
+            if (years == 0)
+            {
+                return currentValue;
+            }
+
+            // Recursive call
+            return CalculateFutureValue(
+                currentValue * (1 + growthRate),
+                growthRate,
+                years - 1
+            );
+        }
+    }
+}
---
+++ Data Structures and Algorithms/Exercise 7 - Financial Forecasting/FinancialForecasting/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace FinancialForecasting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating an object of the ForecastCalculator class
+            ForecastCalculator calculator = new ForecastCalculator();
+
+            // Initial financial data
+            double currentValue = 10000;
+            double growthRate = 0.10;
+            int years = 3;
+
+            // Calculating the future value
+            double futureValue = calculator.CalculateFutureValue(
+                currentValue,
+                growthRate,
+                years
+            );
+
+            Console.WriteLine("Financial Forecast:");
+            Console.WriteLine("Current Value: " + currentValue);
+            Console.WriteLine("Annual Growth Rate: " + (growthRate * 100) + "%");
+            Console.WriteLine("Number of Years: " + years);
+            Console.WriteLine("Predicted Future Value: " + futureValue.ToString("F2"));
+        }
+    }
+}
