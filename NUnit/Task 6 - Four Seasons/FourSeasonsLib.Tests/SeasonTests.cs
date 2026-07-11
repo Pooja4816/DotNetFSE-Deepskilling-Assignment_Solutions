3b0cea37df816be04f31df4991a3df29041f2294
Pooja Mandal <mandalpooja1238@gmail.com>
2026-07-12 01:34:30 +0530

    Complete NUnit Task 6 - Four Seasons TestCaseSource

---
+++ NUnit/Task 6 - Four Seasons/FourSeasonsLib.Tests/SeasonTests.cs
@@ -0,0 +1,62 @@
+﻿using FourSeasonsLib;
+using NUnit.Framework;
+
+namespace FourSeasonsLib.Tests
+{
+    [TestFixture]
+    public class SeasonTests
+    {
+        private Season _season = null!;
+
+        [SetUp]
+        public void Setup()
+        {
+            _season = new Season();
+        }
+
+        // Straightforward way
+        private static readonly object[] SeasonTestCases =
+        {
+            new object[] { "February", "Spring" },
+            new object[] { "March", "Spring" },
+            new object[] { "April", "Summer" },
+            new object[] { "May", "Summer" },
+            new object[] { "June", "Summer" },
+            new object[] { "July", "Monsoon" },
+            new object[] { "August", "Monsoon" },
+            new object[] { "September", "Monsoon" },
+            new object[] { "October", "Autumn" },
+            new object[] { "November", "Autumn" },
+            new object[] { "December", "Winter" },
+            new object[] { "January", "Winter" }
+        };
+
+        [TestCaseSource(nameof(SeasonTestCases))]
+        public void GetSeason_ValidMonth_ReturnsExpectedSeason(
+            string month, string expected)
+        {
+            string actual = _season.GetSeason(month);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        // Alternate way
+        private static IEnumerable<TestCaseData> SeasonCases()
+        {
+            yield return new TestCaseData("February", "Spring");
+            yield return new TestCaseData("April", "Summer");
+            yield return new TestCaseData("July", "Monsoon");
+            yield return new TestCaseData("October", "Autumn");
+            yield return new TestCaseData("December", "Winter");
+        }
+
+        [TestCaseSource(nameof(SeasonCases))]
+        public void GetSeason_DifferentMonths_ReturnsCorrectSeason(
+            string month, string expected)
+        {
+            string actual = _season.GetSeason(month);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
---
+++ NUnit/Task 6 - Four Seasons/FourSeasonsLib/Season.cs
@@ -0,0 +1,37 @@
+﻿namespace FourSeasonsLib
+{
+    public class Season
+    {
+        public string GetSeason(string month)
+        {
+            switch (month.ToLower())
+            {
+                case "february":
+                case "march":
+                    return "Spring";
+
+                case "april":
+                case "may":
+                case "june":
+                    //return "Summer";
+                    return "Wrong Season";
+
+                case "july":
+                case "august":
+                case "september":
+                    return "Monsoon";
+
+                case "october":
+                case "november":
+                    return "Autumn";
+
+                case "december":
+                case "january":
+                    return "Winter";
+
+                default:
+                    return "Invalid Month";
+            }
+        }
+    }
+}
