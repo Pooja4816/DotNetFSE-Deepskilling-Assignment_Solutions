710fb9865e4ecdee5b7f357eed701f7756dadcc3
Pooja Mandal <mandalpooja1238@gmail.com>
2026-07-21 03:03:28 +0530

    Completed Angular Hands-On 2

--- Angular_HandsOn/student-course-portal/src/app/pages/course-list/course-list.ts
+++ Angular_HandsOn/student-course-portal/src/app/pages/course-list/course-list.ts
@@ -1,9 +1,29 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CourseCard } from '../../components/course-card/course-card';
 
 @Component({
   selector: 'app-course-list',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, CourseCard],
   templateUrl: './course-list.html',
-  styleUrl: './course-list.css',
+  styleUrl: './course-list.css'
 })
-export class CourseList {}
+export class CourseList {
+
+  courses = [
+    { id: 1, name: 'Angular', code: 'ANG101', credits: 4 },
+    { id: 2, name: 'Java', code: 'JAVA102', credits: 3 },
+    { id: 3, name: 'C#', code: 'CS103', credits: 4 },
+    { id: 4, name: 'Python', code: 'PY104', credits: 3 },
+    { id: 5, name: 'SQL', code: 'SQL105', credits: 2 }
+  ];
+
+  selectedCourseId: number | null = null;
+
+  onEnroll(courseId: number): void {
+    console.log('Enrolling in course: ' + courseId);
+    this.selectedCourseId = courseId;
+  }
+
+}
--- Angular_HandsOn/student-course-portal/src/app/pages/home/home.ts
+++ Angular_HandsOn/student-course-portal/src/app/pages/home/home.ts
@@ -1,9 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './home.html',
-  styleUrl: './home.css',
+  styleUrl: './home.css'
 })
-export class Home {}
+export class Home implements OnInit, OnDestroy {
+
+  portalName = 'Student Course Portal';
+
+  isPortalActive = true;
+
+  message = '';
+
+  searchTerm = '';
+
+  availableCourses = 10;
+
+  // [property] = One-way binding (Component → DOM)
+  // [(ngModel)] = Two-way binding (DOM ↔ Component)
+
+  ngOnInit(): void {
+    console.log('HomeComponent initialised - courses loaded');
+  }
+
+  ngOnDestroy(): void {
+    console.log('HomeComponent destroyed');
+  }
+
+  onEnrollClick(): void {
+    this.message = 'Enrollment opened!';
+  }
+}
